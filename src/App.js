48b9e4e1f3e4e6e48c0c025602ec56f8bf9f8d1a
cllcnkrt <cllcnkrt@outlook.com>
2021-08-02 20:09:12 +0300

    completed

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
 import "./App.css";
 import { useState, useEffect } from "react";
-import { Button } from "@material-ui/core";
-import { FormControl, InputLabel, Input } from "@material-ui/core";
+import { FormControl, Input } from "@material-ui/core";
 import Message from "./Message";
-import { db } from "./firebase";
+import db from "./firebase";
+import firebase from "firebase";
+import FlipMove from "react-flip-move";
+import SendIcon from "@material-ui/icons/Send";
+import { IconButton } from "@material-ui/core";
+
 function App() {
   const [input, setInput] = useState("");
-  const [messages, setMessages] = useState([
-    {username:'ali',message: 'hey'},
-    {username:'mahmut',message: 'hello'},]);
-  const [username, setUsername] = useState("")
+  const [messages, setMessages] = useState([]);
+  const [username, setUsername] = useState("");
 
-  useEffect(()=>{
-setUsername(prompt("enter a name"))
-  },[])
-  useEffect(()=>{
-    db.collection('messages').onSnapshot(snapshot =>{
-      setMessages(snapshot.docs.map((doc)=>(
-        doc.data()
-      )))
-    })
-      },[])
-    
+  useEffect(() => {
+    db.collection("messages")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) => {
+        setMessages(
+          snapshot.docs.map((doc) => ({ id: doc.id, message: doc.data() }))
+        );
+      });
+  }, []);
+
+  useEffect(() => {
+    setUsername(prompt("enter a name"));
+  }, []);
 
   const onChangehandler = (e) => {
     setInput(e.target.value);
@@ -29,33 +33,47 @@ setUsername(prompt("enter a name"))
 
   const sendMessage = (e) => {
     e.preventDefault();
-    setMessages([...messages, {username: username,text:input}]);
+    db.collection("messages").add({
+      message: input,
+      username: username,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+
     setInput("");
   };
 
   return (
     <div className="App">
+      <img src="https://facebookbrand.com/wp-content/uploads/2018/09/Header-e1538151782912.png?w=100&h=100" />
       <h1>Facebook messenger clone</h1>
       <h2>Welcome {username}</h2>
-      <form action="">
-        {" "}
-        <FormControl>
-          <InputLabel>Enter a message </InputLabel>
-          <Input value={input} onChange={onChangehandler} />
-          <Button
+      <form className="app__form">
+        <FormControl className="app__formControl">
+          <Input
+          className="app__input"
+            placeholder="Enter a message"
+            value={input}
+            onChange={onChangehandler}
+          />
+
+          <IconButton 
+          className="app__iconButton"
             variant="contained"
             color="primary"
             type="submit"
             disabled={!input}
             onClick={sendMessage}
           >
-            Send Messages
-          </Button>
+            <SendIcon />
+          </IconButton>
+          
         </FormControl>
       </form>
-      {messages.map((message, index) => (
-        <Message key={index}  username={username} message={message}/>
-      ))}
+      <FlipMove>
+        {messages.map(({ message, id }) => (
+          <Message key={id} username={username} message={message} />
+        ))}
+      </FlipMove>
     </div>
   );
 }
--- src/Message.js
+++ src/Message.js
@@ -1,27 +1,27 @@
 import { Card, Typography, CardContent } from "@material-ui/core";
-import React from "react";
+import React, { forwardRef } from 'react';
 import "./Message.css";
 
-function Message({ message, username, index}) {
+const  Message= forwardRef(({ message, username},ref) =>{
   const isUser = username === message.username;
 
   return (
-    <div className={`message ${isUser && "message__user"} `}>
+    <div ref={ref} className={`message ${isUser && "message__user"} `}>
       <Card className={isUser ? "message__userCard": "message__guestCard"}>
         <CardContent>
           <Typography
             color="white"
             variant="h5"
             component="h2"
-            key={index}
+            
           >
-            {message.username}: {message.message}
+            {!isUser && `${message.username || 'Unknown User'} says: `} {message.message}
           </Typography>
         </CardContent>
       </Card>
       <h2></h2>
     </div>
   );
-}
+})
 
 export default Message;
